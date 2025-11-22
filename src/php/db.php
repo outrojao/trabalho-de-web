307dee2ea8a4e1f672a84305d7efe4fea8ef8db5
João Victor <joaovictordsantosr@gmail.com>
2025-11-21 21:13:57 -0300

    feat: add php connection to database

---
+++ src/php/db.php
@@ -0,0 +1,13 @@
+<?php
+
+$host = 'db';
+$db   = 'meu_db';
+$user = 'root';
+$pass = 'root';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erro ao conectar ao banco: " . $e->getMessage());
+}
